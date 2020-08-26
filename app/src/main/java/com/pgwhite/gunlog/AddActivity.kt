f941ce1e62450b2dc92b8ccf4ee4b924098263ea
Prescott White <prgwhite@ucsc.edu>
2020-08-25 18:05:44 -0700

    Init commit. Create main activity with two buttons and create activity for adding new gun

---
+++ app/src/main/java/com/pgwhite/gunlog/AddActivity.kt
@@ -0,0 +1,13 @@
+package com.pgwhite.gunlog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+    }
+}
---
+++ app/src/main/java/com/pgwhite/gunlog/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pgwhite.gunlog
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addNewGun(view: View) {
+        val intent = Intent(this, AddActivity::class.java)
+
+        startActivity(intent)
+    }
+}
