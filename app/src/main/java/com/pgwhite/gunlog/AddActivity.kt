0ab4ffa7a3e4ac30fc6051db44238ab482583be5
Prescott White <prgwhite@ucsc.edu>
2020-09-15 16:48:55 -0700

    Add recoil spring option when creating new gun

--- app/src/main/java/com/pgwhite/gunlog/AddActivity.kt
+++ app/src/main/java/com/pgwhite/gunlog/AddActivity.kt
@@ -1,45 +1,56 @@
 package com.pgwhite.gunlog
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
 
-    private lateinit var editMfrView: EditText
-    private lateinit var editModelView: EditText
-    private lateinit var editTotalRoundsView: EditText
-
     private lateinit var gunViewModel: GunViewModel
+
+    private lateinit var gun: Gun
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
 
-        editMfrView = findViewById(R.id.editTextManufacturer)
-        editModelView = findViewById(R.id.editTextModel)
-        editTotalRoundsView = findViewById(R.id.editTextTotalRounds)
-
         gunViewModel = ViewModelProvider(this).get(GunViewModel::class.java)
 
-        val button = findViewById<Button>(R.id.buttonSave)
-        button.setOnClickListener {
+        checkBoxRecoilSpring.setOnClickListener {
+            if (checkBoxRecoilSpring.isChecked) {
+                editTextRecoilRounds.visibility = View.VISIBLE
+            }
+            else {
+                editTextRecoilRounds.visibility = View.INVISIBLE
+            }
+        }
+
+        buttonSave.setOnClickListener {
+            // text
+            val mfrText = editTextManufacturer.text.toString()
+            val modelText = editTextModel.text.toString()
+
+            // ints
             var roundsInt = -1
-            val mfrText = editMfrView.text.toString()
-            val modelText = editModelView.text.toString()
-            if (editTotalRoundsView.text.isNotEmpty()) {
-                roundsInt = editTotalRoundsView.text.toString().toInt()
+            var recoilInt = -1
+            if (editTextTotalRounds.text.isNotEmpty()) {
+                roundsInt = editTextTotalRounds.text.toString().toInt()
+            }
+            if (checkBoxRecoilSpring.isChecked) {
+                if (editTextRecoilRounds.text.isNotEmpty()) {
+                    recoilInt = editTextRecoilRounds.text.toString().toInt()
+                }
             }
 
-            if (mfrText.isEmpty() || modelText.isEmpty() || roundsInt == -1) {
+            if (mfrText.isEmpty() || modelText.isEmpty() || roundsInt == -1 || ((checkBoxRecoilSpring.isChecked) && (recoilInt == -1))) {
                 Toast.makeText(this, R.string.toast_missing_fields, Toast.LENGTH_SHORT).show()
             }
 
             else {
-                val gun = Gun(0, mfrText, modelText, roundsInt)
+                gun = Gun(0, mfrText, modelText, roundsInt, checkBoxRecoilSpring.isChecked, recoilInt)
                 gunViewModel.insert(gun)
                 Toast.makeText(this, R.string.toast_added_gun, Toast.LENGTH_SHORT).show()
                 finish()
--- app/src/main/java/com/pgwhite/gunlog/Gun.kt
+++ app/src/main/java/com/pgwhite/gunlog/Gun.kt
@@ -8,5 +8,7 @@ import androidx.room.PrimaryKey
 class Gun(@PrimaryKey(autoGenerate = true) val id: Int,
           @ColumnInfo(name = "mfr") val mfr: String,
           @ColumnInfo(name = "model") val model: String,
-          @ColumnInfo(name = "rounds_total") var rounds_total: Int) {
+          @ColumnInfo(name = "rounds_total") var rounds_total: Int,
+          @ColumnInfo(name = "semi_handgun_bool") var semi_handgun_bool: Boolean = false,
+          @ColumnInfo(name = "recoil_spring_rounds") var recoil_spring_rounds: Int = 0) {
 }
